3b83c6ed4a2f3e839ad0e45f8b4c6cd5f98d50cc
ZHANG Shijin <zhangshijin@cambricon.com>
2020-03-20 15:38:33 +0800

    Update compilation script

--- prepare_paddle_env.sh
+++ prepare_paddle_env.sh
@@ -1,44 +1,46 @@
-PADDLE_LITE_HOME=/home/dingminghui/paddle/Paddle-Lite
+paddle_home=/home/zhangsan/Paddle-Lite
 if [ $# != 1 ] ; then
     echo "prepare_paddle_env.sh  1"
     echo "arg1: compile or not, 0: not compile src, 1: compile src"
     exit 1
 fi
 
+echo "PADDLE_LITE_HOME is ${PADDLE_LITE_HOME}"
+
+if [ -z "${PADDLE_LITE_HOME}" ]; then
+    echo "PADDLE_LITE_HOME is empty, set to: ${paddle_home}"
+    PADDLE_LITE_HOME="$paddle_home"
+elif [ ! -w "${PADDLE_LITE_HOME}" ]; then
+    echo "PADDLE_LITE_HOME cann't be rw, set to: ${paddle_home}"
+    PADDLE_LITE_HOME="$paddle_home"
+fi
+
 echo "paddle lite home : ${PADDLE_LITE_HOME}"
-if [ ! -d ${PADDLE_LITE_HOME} ];then
-    echo "The dir of PADDLE_LITE_HOME doesn't exist!, please fill in first."
+if [ ! -w ${PADDLE_LITE_HOME} ];then
+    echo "The dir of PADDLE_LITE_HOME doesn't exist or cann't be read! Please set PADDLE_LITE_HOME first."
     exit 1
 fi
 
 if(($1==1));then
     cd ${PADDLE_LITE_HOME}
-      mkdir -p build
-      cd build
-      cmake .. -DWITH_LITE=ON \
-               -DLITE_WITH_LIGHT_WEIGHT_FRAMEWORK=OFF \
-               -DWITH_PYTHON=OFF \
-               -DLITE_WITH_ARM=OFF \
-               -DWITH_GPU=OFF \
-               -DWITH_MKLDNN=OFF \
-               -DLITE_WITH_X86=ON \
-               -DWITH_MKL=ON \
-               -DLITE_WITH_MLU=ON \
-      make -j8
-
+        ./lite/tools/build_mlu.sh build
     cd -
 fi
 
 echo "Finish compiling paddle lite"
-export PADDLE_LINK_PATH="${PADDLE_LITE_HOME}/build/lite/api"
+export PADDLE_LINK_PATH="${PADDLE_LITE_HOME}/build.lite.mlu/lite/api"
 export PADDLE_INC_PATH="${PADDLE_LITE_HOME}/lite/api"
 
+# cp ${PADDLE_LITE_HOME}/build.lite.mlu/lite/api/python/pybind/liblite_pybind.so python/lite_core.so
+
 mkdir -p build
+rm -r build/*
 if [ -d build ];then
     cd build
 else
     exit 1
 fi
+
 cmake ..
 make -j8
 cd ..
