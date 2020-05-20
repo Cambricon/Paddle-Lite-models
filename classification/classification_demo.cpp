30ebe7d3a1a15985404cf84cb0d9bb33637d2159
jiaopu <jiaopu@cambricon.com>
2020-05-20 16:38:25 +0800

    Using TEST_F

--- classification/classification_demo.cpp
+++ classification/classification_demo.cpp
@@ -100,14 +100,6 @@ public:
   }
   void refresh_input(std::vector<int64_t> shape) {
     input_tensor_ = std::move(predictor_->GetInput(0));
-    // char* pPATH = nullptr;
-    /*
-    pPATH = std::getenv("N_CHANGED");
-    if (pPATH != nullptr) {
-      int64_t n = (rand() % 7)+ 1;
-      this->set_i_shape_0(n);
-    }
-    */
     i_shape_ = shape;
     input_tensor_->Resize(i_shape_);
     width_ = i_shape_[3];
@@ -122,162 +114,175 @@ public:
   }
 };
 
-// int main(int argc, char **argv) {
-// TEST(paddle, classification) {
-// std::string input_image_pathes = "/home/zhaoying/imagenet/val_5000.txt";
-// std::string input_image_pathes = "/home/zhaoying/imagenet/val_1000.txt";
-// std::string input_image_pathes = "/home/zhaoying/imagenet/val_100.txt";
-// std::string input_image_pathes = "/projs/systools/zhangshijin/val.txt";
-// std::string input_image_pathes = "/home/zhangmingwei/ws/filelist";
-void test_classification(std::string model_dir) {
-  // std::string model_dir = "/home/dingminghui/paddle/data/ResNet50_quant/";
-  std::string input_image_pathes = "./filelist";
-  std::string label_path = input_image_pathes;
-  std::cout << "model_path:  " << model_dir << std::endl;
-  std::cout << "image and label path:  " << input_image_pathes << std::endl;
-  std::vector<std::vector<int64_t>> changed_shape = {
-      {1, 3, 224, 224}, {4, 3, 224, 224}, {2, 3, 224, 224},
-      {6, 3, 224, 224}, {4, 3, 224, 224}, {9, 3, 224, 224}};
-  int shape_i = 0;
-  char *pPATH = std::getenv("N_CHANGED");
+class classification_test : public testing::Test {
+public:
+  void test() {
+    std::vector<ACCU> accus;
 
-  // Load Labels
-  std::vector<int> labels = load_labels(label_path);
+    // warm up
+    {
+      std::cout << "warm up ....." << std::endl;
+      std::string image_name = pathes[0];
+      cv::Mat input_image = cv::imread(image_name, -1);
+      infer->warm_up(input_image);
+      if (pPATH != nullptr) {
+        infer->refresh_input(changed_shape[shape_i % 6]);
+      } else {
+        infer->refresh_input({BATCH_SIZE, 3, 224, 224});
+      }
+      shape_i++;
+      std::cout << "warm up end" << std::endl;
+    }
 
-  // Set MobileConfig
+    auto start = get_current_us();
+    int index = 0;
+    std::vector<int> batch_labels;
+    batch_labels.reserve(BATCH_SIZE);
+    for (int i = 0; i < pathes.size() - 1; i++) {
+      std::string image_name = pathes[i];
+      std::cout << image_name << std::endl;
+      std::string real_path = image_name;
+      cv::Mat input_image = cv::imread(real_path, -1);
+      printf("process %d th image", i);
+      try {
+        infer->batch(input_image);
+        batch_labels.emplace_back(labels[i]);
+      } catch (cv::Exception &e) {
+        continue;
+      }
+      if (pPATH != nullptr) {
+        if (index % infer->get_i_shape_0() == infer->get_i_shape_0() - 1) {
+          std::vector<RESULT> results = infer->process();
+          for (int j = 0; j < results.size(); ++j) {
+            accus.push_back(get_accu(results[j], batch_labels[j]));
+          }
+          batch_labels.clear();
+          index = -1;
+          infer->refresh_input(changed_shape[shape_i++ % 6]);
+        }
+      } else {
+        if (index % BATCH_SIZE == BATCH_SIZE - 1) {
+          std::vector<RESULT> results = infer->process();
+          for (int j = 0; j < results.size(); ++j) {
+            accus.push_back(get_accu(results[j], batch_labels[j]));
+          }
+          batch_labels.clear();
+          infer->refresh_input({BATCH_SIZE, 3, 224, 224});
+        }
+      }
+      ++index;
+    }
+    auto end = get_current_us();
+    double cur_time_cost = (end - start) / 1000.0f;
+    float fps = (float)(pathes.size() - 1) / (cur_time_cost / 1000.0f);
+    float mean_top1 = 0;
+    float mean_top5 = 0;
+    int total_top1 = 0;
+    int total_top5 = 0;
+    for (size_t i = 0; i < accus.size(); i++) {
+      total_top1 += accus[i].top1;
+      total_top5 += accus[i].top5;
+    }
+    mean_top1 = (float)total_top1 / (float)accus.size();
+    mean_top5 = (float)total_top5 / (float)accus.size();
+    std::cout << "top1 for " << accus.size() << " images: " << mean_top1
+              << std::endl;
+    std::cout << "top5 for " << accus.size() << " images: " << mean_top5
+              << std::endl;
+    std::cout << "fps for " << accus.size() << " images: " << fps << std::endl;
+    std::cout << "average preprocess time :" << infer->avg_preprocess_time()
+              << std::endl;
+    std::cout << "average prediction time :" << infer->avg_prediction_time()
+              << std::endl;
+    std::cout << "average postprocess time :" << infer->avg_postprocess_time()
+              << std::endl;
+    EXPECT_GT(mean_top1, min_top1);
+    EXPECT_GT(mean_top5, min_top5);
+  }
+
+protected:
+  std::unique_ptr<Inferencer_classification> infer;
   CxxConfig config;
-  config.set_model_dir(model_dir);
-  std::vector<Place> valid_places{
-      Place{TARGET(kX86), PRECISION(kFloat)},
-      Place{TARGET(kMLU), PRECISION(kFP16), DATALAYOUT(kNHWC)}
-      /* Place{TARGET(kMLU), PRECISION(kFloat), DATALAYOUT(kNHWC)} */
-  };
-  config.set_valid_places(valid_places);
-
-  config.set_mlu_use_first_conv(use_first_conv);
-  if (use_first_conv) {
-    INPUT_MEAN = {124, 117, 104};
-    INPUT_STD = {59, 57, 57};
-    std::vector<float> mean_vec = INPUT_MEAN;
-    std::vector<float> std_vec = INPUT_STD;
-    config.set_mlu_first_conv_mean(mean_vec);
-    config.set_mlu_first_conv_std(std_vec);
-  }
-
-  config.set_mlu_core_version(MLUCoreVersion::MLU_270);
-  config.set_mlu_core_number(16);
-  config.set_mlu_input_layout(DATALAYOUT(kNHWC));
-
-  std::shared_ptr<PaddlePredictor> predictor =
-      CreatePaddlePredictor<CxxConfig>(config);
-
-  Inferencer_classification infer(predictor, {BATCH_SIZE, 3, 224, 224});
-
-  std::vector<ACCU> accus;
-  std::vector<std::string> pathes = load_image_pathes(input_image_pathes);
-
-  // warm up
-  {
-    std::cout << "warm up ....." << std::endl;
-    std::string image_name = pathes[0];
-    cv::Mat input_image = cv::imread(image_name, -1);
-    infer.warm_up(input_image);
-    if (pPATH != nullptr) {
-      infer.refresh_input(changed_shape[shape_i % 6]);
-    } else {
-      infer.refresh_input({BATCH_SIZE, 3, 224, 224});
+  std::shared_ptr<PaddlePredictor> predictor;
+  std::vector<std::string> pathes;
+  std::vector<int> labels;
+  std::vector<Place> valid_places;
+  char *pPATH;
+  std::vector<std::vector<int64_t>> changed_shape;
+  float min_top1;
+  float min_top5;
+  int shape_i;
+  virtual void SetUp() {
+    shape_i = 0;
+    pPATH = std::getenv("N_CHANGED");
+    pathes = load_image_pathes("./filelist");
+    labels = load_labels("./filelist");
+    valid_places = {Place{TARGET(kX86), PRECISION(kFloat)},
+                    Place{TARGET(kMLU), PRECISION(kFP16), DATALAYOUT(kNHWC)}};
+    changed_shape = {{1, 3, 224, 224}, {4, 3, 224, 224}, {2, 3, 224, 224},
+                     {6, 3, 224, 224}, {4, 3, 224, 224}, {9, 3, 224, 224}};
+    config.set_valid_places(valid_places);
+    config.set_mlu_use_first_conv(use_first_conv);
+    if (use_first_conv) {
+      INPUT_MEAN = {124, 117, 104};
+      INPUT_STD = {59, 57, 57};
+      std::vector<float> mean_vec = INPUT_MEAN;
+      std::vector<float> std_vec = INPUT_STD;
+      config.set_mlu_first_conv_mean(mean_vec);
+      config.set_mlu_first_conv_std(std_vec);
     }
-    shape_i++;
-    std::cout << "warm up end" << std::endl;
-    // std::string real_path = "/home/zhaoying/imagenet/" + image_name;
-    // std::string real_path = "/opt/shared/beta/models_and_data/imagenet/" +
-    // image_name; cv::Mat input_image = cv::imread(real_path, 1);
-    // process(input_image, predictor);
+    config.set_mlu_core_version(MLUCoreVersion::MLU_270);
+    config.set_mlu_core_number(16);
+    config.set_mlu_input_layout(DATALAYOUT(kNHWC));
   }
+  virtual void TearDown() {}
+};
 
-  auto start = get_current_us();
-  int index = 0;
-  std::vector<int> batch_labels;
-  batch_labels.reserve(BATCH_SIZE);
-  for (int i = 0; i < pathes.size() - 1; i++) {
-    std::string image_name = pathes[i];
-    std::cout << image_name << std::endl;
-    // std::string real_path = "/home/zhaoying/imagenet/" + image_name;
-    std::string real_path = image_name;
-    cv::Mat input_image = cv::imread(real_path, -1);
-    // cv::imshow("aaa", input_image);
-    // cv::waitKey(0);
-    printf("process %d th image", i);
-    try {
-      infer.batch(input_image);
-      batch_labels.emplace_back(labels[i]);
-    } catch (cv::Exception &e) {
-      continue;
-    }
-    if (pPATH != nullptr) {
-      if (index % infer.get_i_shape_0() == infer.get_i_shape_0() - 1) {
-        std::vector<RESULT> results = infer.process();
-        for (int j = 0; j < results.size(); ++j) {
-          accus.push_back(get_accu(results[j], batch_labels[j]));
-        }
-        batch_labels.clear();
-        index = -1;
-        infer.refresh_input(changed_shape[shape_i++ % 6]);
-      }
-    } else {
-      if (index % BATCH_SIZE == BATCH_SIZE - 1) {
-        std::vector<RESULT> results = infer.process();
-        for (int j = 0; j < results.size(); ++j) {
-          accus.push_back(get_accu(results[j], batch_labels[j]));
-        }
-        batch_labels.clear();
-        infer.refresh_input({BATCH_SIZE, 3, 224, 224});
-      }
-    }
-    ++index;
-  }
-  auto end = get_current_us();
-  double cur_time_cost = (end - start) / 1000.0f;
-  float fps = (float)(pathes.size() - 1) / (cur_time_cost / 1000.0f);
-  float mean_top1 = 0;
-  float mean_top5 = 0;
-  int total_top1 = 0;
-  int total_top5 = 0;
-  for (size_t i = 0; i < accus.size(); i++) {
-    total_top1 += accus[i].top1;
-    total_top5 += accus[i].top5;
-  }
-  mean_top1 = (float)total_top1 / (float)accus.size();
-  mean_top5 = (float)total_top5 / (float)accus.size();
-  std::cout << "top1 for " << accus.size() << " images: " << mean_top1
-            << std::endl;
-  std::cout << "top5 for " << accus.size() << " images: " << mean_top5
-            << std::endl;
-  std::cout << "fps for " << accus.size() << " images: " << fps << std::endl;
-  std::cout << "average preprocess time :" << infer.avg_preprocess_time()
-            << std::endl;
-  std::cout << "average prediction time :" << infer.avg_prediction_time()
-            << std::endl;
-  std::cout << "average postprocess time :" << infer.avg_postprocess_time()
-            << std::endl;
-  EXPECT_GT(mean_top1, 0.7);
-  EXPECT_GT(mean_top5, 0.9);
+TEST_F(classification_test, resnet50) {
+  config.set_model_dir("/home/dingminghui/paddle/data/ResNet50_quant/");
+  predictor = CreatePaddlePredictor<CxxConfig>(config);
+  infer.reset(
+      new Inferencer_classification(predictor, {BATCH_SIZE, 3, 224, 224}));
+  min_top1 = 0.7;
+  min_top5 = 0.9;
+  test();
 }
 
-TEST(paddle, classification_resnet50) {
-  test_classification("/home/dingminghui/paddle/data/ResNet50_quant/");
+TEST_F(classification_test, resnet101) {
+  config.set_model_dir("/home/jiaopu/model_0515/resnet101_KL_quant/");
+  predictor = CreatePaddlePredictor<CxxConfig>(config);
+  infer.reset(
+      new Inferencer_classification(predictor, {BATCH_SIZE, 3, 224, 224}));
+  min_top1 = 0.7;
+  min_top5 = 0.9;
+  test();
 }
-TEST(paddle, classification_resnet101) {
-  test_classification("/home/jiaopu/model_0515/resnet101_KL_quant/");
+TEST_F(classification_test, mobilenetv2_KL) {
+  config.set_model_dir("/home/jiaopu/model_0515/mobilenetv2_KL_quant/");
+  predictor = CreatePaddlePredictor<CxxConfig>(config);
+  infer.reset(
+      new Inferencer_classification(predictor, {BATCH_SIZE, 3, 224, 224}));
+  min_top1 = 0.7;
+  min_top5 = 0.9;
+  test();
 }
-TEST(paddle, classification_mobilenetV2_KL) {
-  test_classification("/home/jiaopu/model_0515/mobilenetv2_KL_quant/");
+TEST_F(classification_test, googlenet_KL) {
+  config.set_model_dir("/home/jiaopu/model_0515/googlenet_KL_quant/");
+  predictor = CreatePaddlePredictor<CxxConfig>(config);
+  infer.reset(
+      new Inferencer_classification(predictor, {BATCH_SIZE, 3, 224, 224}));
+  min_top1 = 0.7;
+  min_top5 = 0.9;
+  test();
 }
-TEST(paddle, classification_MobileNetV1) {
-  test_classification("/home/jiaopu/model_0515/MobileNetV1_quant/");
-}
-TEST(paddle, classification_googlenet_KL) {
-  test_classification("/home/jiaopu/model_0515/googlenet_KL_quant/");
+TEST_F(classification_test, MobileNetV1) {
+  config.set_model_dir("/home/jiaopu/model_0515/MobileNetV1_quant/");
+  predictor = CreatePaddlePredictor<CxxConfig>(config);
+  infer.reset(
+      new Inferencer_classification(predictor, {BATCH_SIZE, 3, 224, 224}));
+  min_top1 = 0.65;
+  min_top5 = 0.85;
+  test();
 }
 
 int main(int argc, char **argv) {
