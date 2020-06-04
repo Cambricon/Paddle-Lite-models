b37848879e6c81b1776d155ee5ccad678d5b90bc
zhaocai <zhaocai@cambricon.com>
2020-06-04 17:22:42 +0800

    modify compile.sh

--- offline_demo/compile.sh
+++ offline_demo/compile.sh
@@ -1,6 +1,6 @@
 
 compile () {
-g++  -std=c++14 -g -Wall  ./offline_loader.cpp -o offline_loader \
+g++  -std=c++11 -g -Wall  ./offline_loader.cpp -o offline_loader \
 		-L ${NEUWARE_HOME}/lib64 \
       -lcnrt `pkg-config --libs --cflags opencv` -ldl -lstdc++ -O0
 if (($?!=0));then
