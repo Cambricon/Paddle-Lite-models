534b52092505b5ee391fc0bddd3b517c599a4b2d
hfcj <hfcj@cambricon.com>
2020-06-15 13:56:37 +0800

    compile

--- offline_demo/compile.sh
+++ offline_demo/compile.sh
@@ -1,8 +1,9 @@
 
 compile () {
 g++  -std=c++11 -g -Wall  ./offline_loader.cpp -o offline_loader \
-		-L ${NEUWARE_HOME}/lib64 \
-      -lcnrt `pkg-config --libs --cflags opencv` -ldl -lstdc++ -O0
+	    -I $NEUWARE_HOME/include \
+					-L ${NEUWARE_HOME}/lib64 \
+      -lcnrt -lcndrv `pkg-config --libs --cflags opencv` -ldl -lstdc++ -O0
 if (($?!=0));then
   echo "compiling failed!"
   exit 1
